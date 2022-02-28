9850f2037088707bc9e1c2f317392c8cd3f89da9
kicgahi <76626394+kicgahi@users.noreply.github.com>
2022-02-28 12:01:44 +0800

    Update summary.py
    change to historical

--- summary.py
+++ summary.py
@@ -29,12 +29,12 @@ def display():
         with portfolios[column]:
             st.subheader(theme)
             st.markdown("##### **Low Risk**")
-            st.markdown(f"**Expected Return: {round(low_risk[theme][1]*100, 2)}%**")
-            st.markdown(f"**Expected Variance: {round(low_risk[theme][2]*100, 2)}%**")
+            st.markdown(f"**Historical Mean Return: {round(low_risk[theme][1]*100, 2)}%**")
+            st.markdown(f"**Historical Variance: {round(low_risk[theme][2]*100, 2)}%**")
             st.number_input("Your stake: ", min_value=0.0, max_value=1.0, value=1/6, key=theme +"_low")
             st.markdown("##### **High Risk**")
-            st.markdown(f"**Expected Return: {round(high_risk[theme][1]*100, 2)}%**")
-            st.markdown(f"**Expected Variance: {round(high_risk[theme][2]*100, 2)}%**")
+            st.markdown(f"**Historical Mean Return: {round(high_risk[theme][1]*100, 2)}%**")
+            st.markdown(f"**Historical Variance: {round(high_risk[theme][2]*100, 2)}%**")
             st.number_input("Your stake: ", min_value=0.0, max_value=1.0, value=1/6, key=theme +"_high")
 
         column += 1
